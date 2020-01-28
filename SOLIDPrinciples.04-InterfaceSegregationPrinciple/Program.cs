c656ddb1c6cec3a982467d36a3ca525ce3c8d512
hrlinformationtechnologies <hraylong@gmail.com>
2020-01-28 17:38:44 -0500

    OpenClosePrinciple InterfaceSegregationPrinciple

---
+++ SOLIDPrinciples.04-InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLIDPrinciples._04_InterfaceSegregationPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
