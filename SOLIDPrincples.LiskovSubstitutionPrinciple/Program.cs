45715388bed2073c10ed2aea5012159abbde4592
hrlinformationtechnologies <hraylong@gmail.com>
2020-01-28 11:31:29 -0500

    Add project files.

---
+++ SOLIDPrincples.LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace SOLIDPrincples.LiskovSubstitutionPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 5, 7, 9, 8, 1, 6, 4 };
+
+            Calculator sum = new SumCalculator(numbers);
+            Console.WriteLine($"The sum of all the numbers: {sum.Calculate()}");
+
+            Console.WriteLine();
+
+            Calculator evenSum = new EvenNumbersSumCalculator(numbers);
+            Console.WriteLine($"The sum of all the even numbers: {evenSum.Calculate()}");
+        }
+    }
+
+    public class SumCalculator : Calculator
+    {
+        public SumCalculator(int[] numbers) : base(numbers)
+        {
+
+        }
+
+        public override int Calculate() => _numbers.Sum();
+    }
+
+    public class EvenNumbersSumCalculator : Calculator
+    {
+        public EvenNumbersSumCalculator(int[] numbers) : base(numbers)
+        {
+        }
+
+        public override int Calculate() => _numbers.Where(x => x % 2 == 0).Sum();
+    }
+
+    public abstract class Calculator
+    {
+        protected readonly int[] _numbers;
+
+        public Calculator(int[] numbers)
+        {
+            _numbers = numbers;
+        }
+
+        public abstract int Calculate();
+    }
+}
